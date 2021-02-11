aa77f7acd66d25a6bb4895c3260fd8a17cf41f43
zamhsu <41725143+zamhsu@users.noreply.github.com>
2021-02-11 20:12:04 +0800

    新增ChatBot範例專案

---
+++ LineBotSample/Controllers/ChatBotController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace LineBotSample.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ChatBotController : ControllerBase
+    {
+        private readonly ILogger<ChatBotController> _logger;
+
+        public ChatBotController(ILogger<ChatBotController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public IActionResult Webhook()
+        {
+            return Ok();
+        }
+    }
+}
